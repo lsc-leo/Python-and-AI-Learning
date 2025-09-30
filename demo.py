f257a7d144a06e58b8ab0e28d568f2dd6d887180
lsc-leo <leoscholl@web.de>
2025-09-30 23:59:33 +0200

    Creation of a small calculator

--- demo.py
+++ demo.py
@@ -1,10 +1,34 @@
 print("Test!!")
 
 #### first function
-
-def myFirstFunction(x, y):
+def myFirstAddFunction(x, y):
     sum = x + y
-    return sum + 1
+    return sum
 
-### new comments here
-print(myFirstFunction(3,4))
+### second function
+def simpleCalculator(x, y, operator):
+    value = ""
+    if (operator == "+"):
+        value = x + y
+    elif (operator == "-"):
+        value = x - y
+    elif (operator == "*"):
+        value = x * y
+    elif (operator == "/"):
+        value = x / y
+    else:
+        value = "INVALID"
+        
+    if value % 1 == 0:
+        return int(value)
+    return value
+
+
+#print(myFirstAddFunction(3,4))
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+op = input("Operator: ")
+
+res = simpleCalculator(num1, num2, op)
+
+print(res)
