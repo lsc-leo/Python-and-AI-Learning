c8d2f08324a36c78d7d20d28997bf16067113e36
lsc-leo <leoscholl@web.de>
2025-10-03 18:14:10 +0200

    Smooth version with using ::-1 of doublecolon op

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,22 +1,33 @@
 ### Palindrome checker
 ### "racecar" -> true, "nurses run" -> true, "kamel" -> false
+def my_palindromeVersion():
+    inputStr = input("Please input the word or sentence to be checked: ")
+    newAr = ""
+    inputLength = len(inputStr)
 
-inputStr = input("Please input the word or sentence to be checked: ")
-newAr = ""
-inputLength = len(inputStr)
-
-while inputLength > 0:
-    inputLength -= 1
-    newAr += inputStr[inputLength]
-    
-if inputStr.lower().replace(" ", "") == newAr.lower().replace(" ", ""):
-    print(f"{inputStr} is a palindrome")
-else:
-    print(f"{inputStr} is not a palindrome")
+    while inputLength > 0:
+        inputLength -= 1
+        newAr += inputStr[inputLength]
+        
+    if inputStr.lower().replace(" ", "") == newAr.lower().replace(" ", ""):
+        print(f"{inputStr} is a palindrome")
+    else:
+        print(f"{inputStr} is not a palindrome")
 
 #F-Strings are powerful
-def weirdStringStuff():
+def weirdStringStuff(inputStr):
     amount = 12
     ### if I now add new stuff here and push it to NewFeatures, then main will be behind and I need to pull from NewFeatures once I switch path
     output = f"{inputStr} has exactly {amount:.2f} dollars, which is half of {amount * 2:.2f}."
     print(output)
+
+# Online variant, smoother
+def online_palindrome_version(word):
+  
+  reversed_word = word[::-1]
+
+  if reversed_word == word:
+    return True
+  return False
+
+print(online_palindrome_version(input("Test this word: ")))
