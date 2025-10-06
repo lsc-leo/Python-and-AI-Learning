1249d5dac37bda44ee1efc6ddc75de9e3fa303f1
lsc-leo <leoscholl@web.de>
2025-10-06 23:19:27 +0200

    Creation of row rule in sudoku solver and some learnings about arrays

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,6 +5,7 @@ rules:
 - each 3x3 square can only contain each digit once
 '''
 
+
 board = [[0, 0, 3, 0, 0, 0, 0, 0, 7],
          [4, 2, 0, 0, 9, 1, 0, 0, 0],
          [0, 1, 5, 0, 6, 8, 3, 0, 0],
@@ -18,6 +19,22 @@ board = [[0, 0, 3, 0, 0, 0, 0, 0, 7],
 #does this print? - yes, but inflexible
 #for row in board:
 #    print(row)
+
+#row can only contain each digit once (apart from 0)
+def row_rule(inputRow):
+    my_set = set()
+    for num in inputRow:
+        my_set.add(num)
+    my_set.remove(0)
+    list_values = [val for val in inputRow if val != 0] # my first list comprehension
+    if len(my_set) != len(list_values):
+        print(f"Length of the set: {len(my_set)} and the values: {my_set}")
+        print(f"Length of the list comprehended list: {len(list_values)} and the values:{list_values}")
+        return "ERROR IN ROW"
+    return True
+    
+checkFirstRow = row_rule([0, 0, 3, 0, 0, 3, 0, 0, 7])
+print(checkFirstRow)
 
 def normal_board():
     for oInd, row in enumerate(board):
